fb12eea1aed25289e783a57e3ece4c22557a6b7d
gondiaz <gonzalodiazlopez10@gmail.com>
2020-02-24 18:05:49 +0100

    Exercise1 code

--- G4Basic/src/DetectorConstruction.cpp
+++ G4Basic/src/DetectorConstruction.cpp
@@ -50,41 +50,117 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 
   world_logic_vol->SetVisAttributes(G4VisAttributes::Invisible);
 
-  // SHIELDING /////////////////////////////////////////////
+  // CARITA ////////
+  G4double R = 10.*m;
+  G4double H = 1. *m;
 
-  G4String shield_name   = "SHIELDING";
-  G4double shield_diam   = 10.*m;
-  G4double shield_height = 10.*m;
+  // CIL0 //////////////////
+  G4Tubs* cil0_solid =
+    new G4Tubs("CIL0", 2.*R/3., R, H, 0, 360*deg);
 
-  G4Tubs* shield_solid_vol =
-    new G4Tubs(shield_name, 0., shield_diam/2., shield_height/2., 0., 360*deg);
-
-  G4LogicalVolume* shield_logic_vol =
-    new G4LogicalVolume(shield_solid_vol,
+  G4LogicalVolume* cil0_logic =
+    new G4LogicalVolume(cil0_solid,
                         G4NistManager::Instance()->FindOrBuildMaterial("G4_WATER"),
-                        shield_name);
+                        "CIL0");
 
-  G4RotationMatrix* rotX = new G4RotationMatrix();
-  rotX->rotateX(90*deg);
-
-  new G4PVPlacement(rotX, G4ThreeVector(0.,0.,0.),
-                    shield_logic_vol, shield_name, world_logic_vol,
-                    false, 0, true);
-
-  // DETECTOR //////////////////////////////////////////////
-
-  G4String detector_name = "DETECTOR";
-  G4double detector_size = 2.*m;
-
-  G4Box* detector_solid_vol =
-    new G4Box(detector_name, detector_size/2., detector_size/2., detector_size/2.);
-
-  G4LogicalVolume* detector_logic_vol =
-    new G4LogicalVolume(detector_solid_vol, EnrichedXenon(), detector_name);
+  // G4RotationMatrix* rotX = new G4RotationMatrix();
+  // rotX->rotateX(90*deg);
 
   new G4PVPlacement(0, G4ThreeVector(0.,0.,0.),
-                    detector_logic_vol, detector_name, shield_logic_vol,
+                    cil0_logic, "CIL0", world_logic_vol,
                     false, 0, true);
+
+  // CIL1 //////////////////
+  G4Tubs* cil1_solid =
+    new G4Tubs("CIL1", 0, 2.*R/3., H, 0, 180*deg);
+
+  G4LogicalVolume* cil1_logic =
+    new G4LogicalVolume(cil1_solid,
+                        G4NistManager::Instance()->FindOrBuildMaterial("G4_WATER"),
+                        "CIL1");
+
+  new G4PVPlacement(0, G4ThreeVector(0.,0.,0.),
+                    cil1_logic, "CIL1", world_logic_vol,
+                    false, 0, true);
+
+  // OJOD //////////////////
+  G4Tubs* ojod_solid =
+    new G4Tubs("ojod", 0, R/7., H, 0, 360*deg);
+
+  G4LogicalVolume* ojod_logic =
+    new G4LogicalVolume(ojod_solid,
+                        G4NistManager::Instance()->FindOrBuildMaterial("G4_WATER"),
+                        "ojod");
+
+  G4double pos_x = R/2.* std::cos(55.*deg);
+  G4double pos_y = R/2.* std::sin(55.*deg);
+  G4double pos_z = 0.;
+
+  new G4PVPlacement(0, G4ThreeVector(pos_x, pos_y, pos_z),
+                    ojod_logic, "ojod", world_logic_vol,
+                    false, 0, true);
+
+  // OJOI //////////////////
+  G4Tubs* ojoi_solid =
+    new G4Tubs("ojoi", 0, R/7., H, 0, 360*deg);
+
+  G4LogicalVolume* ojoi_logic =
+    new G4LogicalVolume(ojoi_solid,
+                        G4NistManager::Instance()->FindOrBuildMaterial("G4_WATER"),
+                        "ojoi");
+
+  new G4PVPlacement(0, G4ThreeVector(-pos_x, pos_y, pos_z),
+                    ojoi_logic, "ojoi", world_logic_vol,
+                    false, 0, true);
+
+  // boca //////////////////
+  G4Tubs* boca_solid =
+    new G4Tubs("boca", 0, 3.*R/8., H, 180*deg, 180*deg);
+
+  G4LogicalVolume* boca_logic =
+    new G4LogicalVolume(boca_solid,
+                        G4NistManager::Instance()->FindOrBuildMaterial("G4_WATER"),
+                        "boca");
+
+  new G4PVPlacement(0, G4ThreeVector(0, -3.*R/8.+R/7., 0),
+                    boca_logic, "boca", world_logic_vol,
+                    false, 0, true);
+
+  // SHIELDING /////////////////////////////////////////////
+
+  // G4String shield_name   = "SHIELDING";
+  // G4double shield_diam   = 10.*m;
+  // G4double shield_height = 10.*m;
+  //
+  // G4Tubs* shield_solid_vol =
+  //   new G4Tubs(shield_name, 0., shield_diam/2., shield_height/2., 0., 360*deg);
+  //
+  // G4LogicalVolume* shield_logic_vol =
+  //   new G4LogicalVolume(shield_solid_vol,
+  //                       G4NistManager::Instance()->FindOrBuildMaterial("G4_WATER"),
+  //                       shield_name);
+  //
+  // G4RotationMatrix* rotX = new G4RotationMatrix();
+  // rotX->rotateX(90*deg);
+  //
+  // new G4PVPlacement(rotX, G4ThreeVector(0.,0.,0.),
+  //                   shield_logic_vol, shield_name, world_logic_vol,
+  //                   false, 0, true);
+  //
+  // // DETECTOR //////////////////////////////////////////////
+  //
+  // G4String detector_name = "DETECTOR";
+  // G4double detector_size = 2.*m;
+  //
+  // G4Box* detector_solid_vol =
+  //   new G4Box(detector_name, detector_size/2., detector_size/2., detector_size/2.);
+  //
+  // G4LogicalVolume* detector_logic_vol =
+  //   new G4LogicalVolume(detector_solid_vol, EnrichedXenon(), detector_name);
+  //
+  // new G4PVPlacement(0, G4ThreeVector(0.,0.,0.),
+  //                   detector_logic_vol, detector_name, shield_logic_vol,
+  //                   false, 0, true);
 
   //////////////////////////////////////////////////////////
 
